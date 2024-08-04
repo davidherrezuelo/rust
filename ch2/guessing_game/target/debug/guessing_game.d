e7152d4b84273b0e25aecdb608bca64a81fcd36b
unknown <dherrezuelop@gmail.com>
2024-08-04 21:35:18 +0200

    up to chapter 3

--- ch2/guessing_game/target/debug/guessing_game.d
+++ ch2/guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-C:\Users\dherr\OneDrive\ -\ UVa\Escritorio\programacion\rust\ch2\guessing_game\target\debug\guessing_game.exe: C:\Users\dherr\OneDrive\ -\ UVa\Escritorio\programacion\rust\ch2\guessing_game\src\main.rs
+C:\Users\David\Desktop\programacion\rust\rust\ch2\guessing_game\target\debug\guessing_game.exe: C:\Users\David\Desktop\programacion\rust\rust\ch2\guessing_game\src\main.rs
